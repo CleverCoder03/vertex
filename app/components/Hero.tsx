9f3b7c518ad637ea172c246ba6590f29c35e57b3
Senpai <clevercoder0307@gmail.com>
2025-11-19 23:01:08 +0530

    updated hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -3,15 +3,19 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div>
-
       {/* Hero Headline */}
-      <div className="py-4 lg:py-8 uppercase font-abc-marist font-medium text-[11.5vw] leading-none flex flex-col gap-14">
-        <div className="flex flex-col">
-          <h1>Investing</h1>
-          <h1 className="text-right">In</h1>
+      <div className="py-4 lg:py-8 uppercase font-abc-marist font-medium text-[11.5vw] md:text-[10.5vw] leading-none md:leading-[0.8] flex flex-col gap-14 md:gap-20 lg:gap-[12vw]">
+        <div className="flex flex-col md:gap-3">
+          <div className="md:flex md:justify-between">
+            <h1>Investing</h1>
+            <h1 className="text-right">In</h1>
+          </div>
+          <div>
+            <h1 className="hidden md:block text-right">Tomorrow's</h1>
+          </div>
         </div>
         <div>
-          <h1>Tomorrow's</h1>
+          <h1 className="md:hidden">Tomorrow's</h1>
           <h1>spaces</h1>
         </div>
       </div>
@@ -26,7 +30,13 @@ const Hero = () => {
       {/* Short Para */}
       <div className="font-abc-marist text-lg py-5 lg:py-8">
         <p className="uppercase tracking-wide">Vertex</p>
-        <p  className="indent-15 leading-none mt-4 text-justify">At Vertex Real Estate, we create and invest in spaces where life unfolds. We focus on transforming operational assets into robust platforms that bring together people, properties, and infrastructure. We build resilient places that serve the evolving needs of our customers and capital.</p>
+        <p className="indent-15 leading-none mt-4 text-justify">
+          At Vertex Real Estate, we create and invest in spaces where life
+          unfolds. We focus on transforming operational assets into robust
+          platforms that bring together people, properties, and infrastructure.
+          We build resilient places that serve the evolving needs of our
+          customers and capital.
+        </p>
       </div>
     </div>
   );
